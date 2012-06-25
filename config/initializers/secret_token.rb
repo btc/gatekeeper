ea4dbf4762f498e2782756f2e134c4c04f21718c
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-06-25 17:59:47 -0400

    rails new vern --database=postgresql -T no tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vern::Application.config.secret_token = '50794c2e7d21e66375aeff507ad41da29cbf13a8b0448593f2850da79ad794cb88f53412c53cdb47fabf87ac04db8c771a164e4113c5cc6a302ca7d3962cbf03'
