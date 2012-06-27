8a2226697f7d4d110cda3ce8213e8de8acb4d8be
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-06-27 02:20:49 -0400

    better user, role, and admin factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,20 @@
 FactoryGirl.define do
 
+  sequence :email do |n|
+    "person#{n.to_s + SecureRandom.hex}@example.com"
+  end
+
   factory :user do
-    salt = SecureRandom.hex
-    email "user" + salt + "@example.com"
+    email
     password SecureRandom.hex
+    factory :admin do
+      admin true
+    end
   end
 
   factory :role do
+    name 'name'
+    user
   end
 
 end
