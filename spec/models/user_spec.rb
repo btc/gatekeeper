b26c8763272ccc08aa2c304f3c14db12977dff0d
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-06-27 02:21:09 -0400

    better user testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,23 @@ require 'spec_helper'
 
 describe User do
 
-  before do
+  it "creation can be simulated with FactoryGirl" do
     u = FactoryGirl.create(:user)
   end
 
-  it "creation can be simulated with FactoryGirl" do
+  it "is not an admin when first created" do
+    u = FactoryGirl.create(:user)
+    u.is_admin?.should be_false
+  end
+
+  it "becomes an admin when User.admin= assignment is performed" do
+    u = FactoryGirl.create(:user)
+    u.admin = true
+    u.is_admin?.should be_true
+  end
+
+  it "has a FactoryGirl admin alias defined in factories.rb" do
+    a = FactoryGirl.create(:admin)
   end
 
 end
