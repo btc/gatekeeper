67685b9e6c04477c2ac11da73fca217c4f910706
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-06-27 02:27:57 -0400

    include devise helpers for controller testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -60,14 +60,6 @@ require 'capybara/rspec'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -77,4 +69,8 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  # include devise helpers to do controller testing
+  config.include Devise::TestHelpers, :type => :controller
+
 end
