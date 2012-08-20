e933da120263dfca1ad7a404f9f90d880e14a5a3
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-20 17:59:54 -0400

    add notes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//  THINGS REQUIRED BY THE APPLICATION
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require bootstrap
+//  APPLICATION FUNCTIONALITY
 //= require guests
 //= require webcam
 //= require guest_form
-
