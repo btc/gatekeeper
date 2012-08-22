8f9d38a8e42897855e34b91d8a98c0bfa58c7b1e
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-22 04:31:06 -0400

    add confirmable to devise user and enable mailer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120820211812) do
+ActiveRecord::Schema.define(:version => 20120822074911) do
 
   create_table "committees", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -100,8 +100,13 @@ ActiveRecord::Schema.define(:version => 20120820211812) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.integer  "guest_id"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
