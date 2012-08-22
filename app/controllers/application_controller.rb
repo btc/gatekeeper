ecb38bf904750dbe46b35449049bf8a5f1f405d5
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-22 05:44:22 -0400

    add comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
+  before_filter :authenticate_user! # user must be logged in to view any page
 end
