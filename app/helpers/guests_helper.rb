7ed3326c620bd615ba57cedd6531acd7250591c0
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-22 19:09:33 -0400

    define url_for_profile_pic helper (depends on paperclip, guest model, and avatar)

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -1,2 +1,10 @@
 module GuestsHelper
+  def url_for_profile_pic(guest, size)
+    if guest.photos.first.try(:image)
+      guest.photos.first.image.url(:thumb)
+    else
+      url_for('/assets/placeholder_avatar_square.png')
+    end
+  end
+
 end
