03d6976f88da69729e6dca8f4c8a9e6c0ad413c6
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-22 05:56:29 -0400

    redirect unauthorized requests back to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
   before_filter :authenticate_user! # user must be logged in to view any page
   check_authorization unless: :devise_controller?               # every request must go through cancan
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
 end
