edfe8214db635ac01a6baac7a6911c2ee0127e65
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-22 14:13:47 -0400

    move environment loading to preform block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,21 +7,12 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-
-
-end
-
-Spork.each_run do
-  # This code will be run each time you run your specs.
-  # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'capybara/rspec'
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
@@ -39,9 +30,18 @@ Spork.each_run do
     config.include  Devise::TestHelpers,  :type => :controller
     config.extend   ControllerMacros,     :type => :controller
 
-    # don't run tests that require auth
-    config.filter_run_excluding auth: true
   end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+
+  # don't run tests that require auth
+  config.filter_run_excluding auth: true
 
   FactoryGirl.reload
 end
