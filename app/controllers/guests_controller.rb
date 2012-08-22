435d4f7397b1b55725c381d4de6183a4f87d25f9
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-22 18:40:08 -0400

    return guests in alphabetic order by first name

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -4,7 +4,7 @@ class GuestsController < ApplicationController
   # GET /guests
   # GET /guests.json
   def index
-    @guests = Guest.all
+    @guests = Guest.order('first_name ASC').all
 
     respond_to do |format|
       format.html # index.html.erb
