045e13316fb04050c44d4c1f980cfbf309d4f9fa
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-22 05:44:47 -0400

    force every request through cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user! # user must be logged in to view any page
+  check_authorization unless: :devise_controller?               # every request must go through cancan
 end
