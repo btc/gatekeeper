3c9e03d2a50861147f5ba0325f7fb201a8f2aea6
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-24 21:08:18 -0400

    fix url

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, :git
-set :repository, 'ssh://git@github.com:brianholderchow/zen-tech.git'
+set :repository, 'git@github.com:brianholderchow/zen-tech.git'
 set :branch, 'master'
 
 default_run_options[:pty] = true
