23d062580ee29a669577aa58bc8373a0a8283f06
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-28 02:03:16 -0400

    support NL date parsing in GuestList model

--- app/models/guest_list.rb
+++ app/models/guest_list.rb
@@ -1,3 +1,5 @@
+require 'chronic'
+
 class GuestList < ActiveRecord::Base
   attr_accessible :date, :owner_id, :creator_id, :event_id
 
@@ -8,6 +10,14 @@ class GuestList < ActiveRecord::Base
   has_many :guests, through: :invitations
 
   validates_presence_of :date, :owner, :creator
+  before_save :parse_date
 
   has_paper_trail
+
+  def parse_date
+    if self.date_before_type_cast
+      self.date = Chronic.parse(self.date_before_type_cast, context: :future)
+    end
+  end
+
 end
