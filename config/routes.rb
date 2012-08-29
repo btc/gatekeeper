3fd7260e84995a52c619ba0af96c222aea7aed3f
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-29 16:04:21 -0400

    home page is guests index

--- config/routes.rb
+++ config/routes.rb
@@ -71,7 +71,7 @@ Vern::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'root#index'
+  root :to => 'guests#index'
 
   # See how all your routes lay out with "rake routes"
 
