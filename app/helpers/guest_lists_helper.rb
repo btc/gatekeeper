f3f892603e6576ae46221c319a27f8511f7a1ee6
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-29 04:08:50 -0400

    define date helpers for BIP tips

--- app/helpers/guest_lists_helper.rb
+++ app/helpers/guest_lists_helper.rb
@@ -1,2 +1,21 @@
 module GuestListsHelper
+  # returns a date object that represents the
+  def this_friday
+    day = Date.today
+    until day.friday?
+      day = day.next
+    end
+    day
+  end
+
+  def relative_date(date)
+    n = (date - Date.today).to_int
+    case
+    when n == 0 then 'today'
+    when n > 0 then "in #{n} days"
+    when n == -1 then '1 day ago'
+    else "#{-n} days ago"
+    end
+    # TODO FIXME handle case where it's like 3 am
+  end
 end
