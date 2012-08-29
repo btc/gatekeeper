0ae2a028c73a77b2c670c20446744bb6136df8bb
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-29 18:31:11 -0400

    use github ip

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, :git
-set :repository, 'git@github.com:brianholderchow/zen-tech.git'
+set :repository, 'git@207.97.227.239:brianholderchow/zen-tech.git'
 set :branch, 'master'
 
 default_run_options[:pty] = true
