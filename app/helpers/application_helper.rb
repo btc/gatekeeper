fdf87b6a64e1f824134df027978eed955275f1ff
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-30 09:54:04 -0400

    add optional other_condition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def active_if(path)
-    if current_page?(path)
+  def active_if(path, other_condition = true)
+    if current_page?(path) && other_condition
       { class: 'active' }
     else
       {}
