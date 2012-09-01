7950f011174a64fd8f774fa3162beb4ca08e8d35
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-09-01 19:57:21 -0400

    include flash helper directly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
       {}
     end
   end
+
+  def bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      type = :success if type == :notice
+      type = :error   if type == :alert
+      text = content_tag(:div, link_to("x", "#", :class => "close", "data-dismiss" => "alert") + message, :class => "alert fade in alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
