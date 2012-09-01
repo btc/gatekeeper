6fd06fc1db63991951f3e63903d8620a49af460e
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-08-31 22:57:21 -0400

    it should have tests for door attendant roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,27 +6,30 @@ describe User do
     u = FactoryGirl.create(:user)
   end
 
-  it "is not an admin when first created" do
+  it "is not an admin when first created"
+  it "becomes an admin when User.admin= assignment is performed"
+  it "has a FactoryGirl admin alias defined in factories.rb"
+  it "creates an admin using only the specified factory"
+
+  it 'should have a method that lets you append a new role' do
     u = FactoryGirl.create(:user)
-    u.is_admin?.should be_false
+    u.add_role!(:admin)
+    u.has_role?(:admin).should eq(true)
   end
 
-  it "becomes an admin when User.admin= assignment is performed" do
+  it "shouldn't have a role if i didn't add it" do
     u = FactoryGirl.create(:user)
-    u.admin = true
-    u.is_admin?.should be_true
+    u.has_role?(:admin).should eq(false)
   end
 
-  it "has a FactoryGirl admin alias defined in factories.rb" do
-    a = FactoryGirl.create(:admin)
-  end
+  it 'should have a door_attendant role' do
+    u = FactoryGirl.create :user
+    u.add_role! :door_attendant
+    u.save
 
-  it "creates an admin using only the specified factory" do
-    a = FactoryGirl.create(:admin)
-    a.is_admin?.should be_true
+    u.has_role?(:door_attendant).should eq(true)
   end
 
   it { should belong_to(:guest) }
   it { should have_and_belong_to_many(:committees) }
-
 end
