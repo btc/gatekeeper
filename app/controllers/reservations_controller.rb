6c8bf63bb651f7b30ce236b42de32321f858586b
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-09-03 22:01:14 -0400

    return anon scope instead of array

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,7 +3,7 @@ class ReservationsController < ApplicationController
   # GET /reservations
   # GET /reservations.json
   def index
-    @reservations = Reservation.all
+    @reservations = Reservation.scoped
 
     respond_to do |format|
       format.html # index.html.erb
