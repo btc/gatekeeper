01a35996f4183f44b90607e549df133dc0ee16d1
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-09-03 22:04:01 -0400

    check that mass assignment settings are good

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -6,4 +6,8 @@ describe Reservation do
   it { should belong_to(:guest_list) }
   it { should belong_to(:guest) }
   it { should have_db_column(:date) }
+
+  it { should allow_mass_assignment_of(:guest_list_id) }
+  it { should allow_mass_assignment_of(:table_id) }
+  it { should allow_mass_assignment_of(:guest_id) }
 end
