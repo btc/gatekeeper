4727701319b27bc291fb2f47db853bdcf63fedc6
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-09-04 20:00:22 -0400

    add full name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
-    :first_name, :last_name
+    :first_name, :last_name, :full_name
 
   has_many :roles, inverse_of: :user
   has_and_belongs_to_many :committees
@@ -34,6 +34,6 @@ class User < ActiveRecord::Base
   end
 
   def to_s
-    self.email
+    self.full_name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120903220346) do
+ActiveRecord::Schema.define(:version => 20120904235810) do
 
   create_table "committees", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(:version => 20120903220346) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "full_name"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
