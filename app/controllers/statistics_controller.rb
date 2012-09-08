f56f3e48e3c3ab1766dab7363cfbba01217149f9
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2012-09-08 03:02:29 -0400

    basic stats

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,5 +1,18 @@
 class StatisticsController < ApplicationController
   def index
     authorize! :read, :statistics
+    @guest_lists = GuestList.tonight.approved
+
+    @n_guests_on_lists_tonight = @guest_lists.inject(0) do |sum, list|
+      sum + list.invitations.count
+    end
+
+    @n_check_ins_tonight = @guest_lists.inject(0) do |sum, list|
+      sum + list.invitations.where('redeemed = ?', true).count
+    end
+
+    @pct_checked_in_tonight = (@n_check_ins_tonight.to_f / @n_guests_on_lists_tonight) * 100
+
+    @committees = Committee.all
   end
 end
